dbe3992bce9e2716e888a0671df77d2f85a6fd6c
Luis Vizk <luisv4416@gmail.com>
2016-04-08 14:08:15 -0500

    Agregada funcion para dividir

---
+++ div.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+float div(int a,int b);
+
+float div(int a,int b)
+{
+  float res;
+  res = a/b;
+  return (res);
+}
--- main.c
+++ main.c
@@ -2,36 +2,36 @@
 
 extern float suma(int a, int b);
 extern float mult(int a, int b);
+extern float div(int a, int b);
 
 int main(void)
 {
-	int val1, val2;
-	float res;
-	int opc;
-
-	printf(" 1-suma, 2-mult\n");
-	scanf("%d",&opc);
-	getchar();
-	printf("valor 1\n");
-	scanf("%d",&val1);
-	printf("valor 2\n");
-	scanf("%d",&val2);
+  int val1, val2;
+  float res;
+  int opc;
+  
+  printf(" 1-suma, 2-mult, 3-div\n");
+  scanf("%d",&opc);
+  getchar();
+  printf("valor 1\n");
+  scanf("%d",&val1);
+  printf("valor 2\n");
+  scanf("%d",&val2);
 	
-	if ( opc == 1)
-	{
-		res = suma(val1,val2);
-	}
-	else
-        {
-        	if (opc == 2)
-        	{
-			res = mult(val1,val2);
-		}
-		else
-		  printf ("opcion no valida \n");
-	}
-    printf("el valor es %f \n",res);
-        
-        return 0;
-	
+  if ( opc == 1)
+    {
+      res = suma(val1,val2);
+    }
+  else if (opc == 2)
+    {
+      res = mult(val1,val2);
+    }
+  else if (opc == 3)
+    {
+      res = div(val1,val2);
+    }
+  else
+    printf ("opcion no valida \n");
+  printf("el valor es %f \n",res);
+  return 0;	
 }
