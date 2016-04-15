2dfd2f41eb8080579ff681df757cf4c19e321ba0
eortizl <edgar.ortizl@ibero.mx>
2016-04-15 14:11:42 -0500

    Delete divicion.c
    Borre este archivo, ya existe division.c

--- divicion.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>/*Se inicializan librerias*/
-
-float division(int a, int b){
-    float c;
-    c = (float)a / b; /*Realizo laoperacion con el cast*/
-    return c;/*Se regresa el resultado*/
-}
