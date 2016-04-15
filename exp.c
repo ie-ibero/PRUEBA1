a5dda9b3b87fc0e569a2bd60b0fef14684bb2ecf
GerardoB <ic15gba@antares.dci.uia.mx>
2016-04-15 13:31:41 -0500

    Calcula la exponencial mediante la serie de Taylor
    Se modifico la funcion para calcular la exponencial mediante la serie de taylor con 30 iteraciones.
    Se podria quitar el include de math.h si es necesario.

--- exp.c
+++ exp.c
@@ -3,9 +3,26 @@
 
 float expo(float num)
 {
-  float res;
+  int i=2, j;
+  float res,pot,fact=0;
 
-  res = expf(num);
-
+  res = 1 + num; 
+  pot = num;
+  
+  for(i; i < 30; i++)
+    {
+      fact = i;
+      for(j=i-1; j!=0; j--)
+	fact = fact * j;
+      printf("%f:",fact);
+      
+      for(j=1; j != i; j++)
+	pot = pot * num;
+      printf("%f\n",pot);
+      
+      fact = pot/fact;
+      res = res + fact;
+    }
+  
   return res;
 }
